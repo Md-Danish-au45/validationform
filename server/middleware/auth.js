5d7e7d5cb6e9917e18ec8337d07f5915f4403a6e
MD Danish <82753592+Md-Danish-au45@users.noreply.github.com>
2023-11-18 22:51:47 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,27 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// // Define your JWT secret key
-// const jwtSecret = 'danish';
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.headers.authorization;
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Access denied. Token is missing' });
-//   }
-
-//   try {
-//     // Verify the token and decode it
-//     const decoded = jwt.verify(token.split(' ')[1], jwtSecret);
-//     req.email = decoded.email; // Assuming the email is stored in the JWT payload
-//     next();
-//   } catch (error) {
-//     console.error('Error verifying token:', error);
-//     return res.status(401).json({ message: 'Invalid token', error: error.message });
-//   }
-// };
-
-// module.exports = verifyToken;
 const jwt = require("jsonwebtoken");
 
 // Define your JWT secret key
